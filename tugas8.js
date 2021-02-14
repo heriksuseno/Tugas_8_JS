65f70246c9e1ee003e182e86be6e7b44537bb275
heriksuseno <>
2021-02-14 17:12:04 +0700

    Tugas 8 JS untuk NIOMIC

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+var biodata = {
+  nama : "Herik Suseno",
+  tempat : "Kuningan",
+  tgl : "21 Januari 1991",
+  agama : "Islam",
+  kegiatan : "Belajar Coding",
+}
+biodata.kegiatan = "Belajar Akunting";
+biodata.tempat = "Jawa Barat";
+biodata.sekolah = "NIOMIC"
+console.log(biodata)
